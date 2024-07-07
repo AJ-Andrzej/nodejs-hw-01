d0ae4272929e2853c338a37840c855c66039e3d7
AJ-Andrzej <aayaschenko@gmail.com>
2024-07-07 15:12:34 +0200

    add-generate

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,25 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  const generateContactsData = [];
+  for (let i = 0; i < number; i += 1) {
+    const data = createFakeContact();
+    generateContactsData.push(data);
+  }
+  try {
+    await fs.writeFile(
+      PATH_DB,
+      JSON.stringify(generateContactsData, undefined, 2),
+      {
+        encoding: 'utf8',
+      },
+    );
+    console.log('done');
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 generateContacts(5);
