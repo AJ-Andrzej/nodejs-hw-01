5d308cefe1dda01078d660c0b6f386dab301953a
AJ-Andrzej <aayaschenko@gmail.com>
2024-07-07 15:45:41 +0200

    add-contact

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,23 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-export const addOneContact = async () => {};
+export const addOneContact = async () => {
+  try {
+    const contacts = JSON.parse(
+      await fs.readFile(PATH_DB, { encoding: 'utf8' }),
+    );
+    const newContact = createFakeContact();
+
+    contacts.push(newContact);
+
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, undefined, 2), {
+      encoding: 'utf8',
+    });
+    console.log('Successfully added contact');
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 addOneContact();
